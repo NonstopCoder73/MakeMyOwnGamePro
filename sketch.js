a29b5b89b9eb2d78e82cb38aa91955df56b9d3c6
Chief Rocker <amartya.shenoy@gmail.com>
2021-04-22 15:39:07 +0530

    My Own Game

--- sketch.js
+++ sketch.js
@@ -1,44 +1,120 @@
-var canvas, backgroundImage;
+let board = [['', '', ''], ['', '', ''], ['', '', '']];
 
-var gameState = 0;
-var playerCount;
-var allPlayers;
-var distance = 0;
-var database;
+let players = ['X', 'O'];
+let currentPlayer;
+let avail = [];
 
-var form, player, game;
+function preload() {
 
-var cars, car1, car2, car3, car4;
-
-var track, car1_img, car2_img, car3_img, car4_img;
-
-function preload(){
-  track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
 }
 
-function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+function setup() {
+  canvas = createCanvas(400, 400);
+
+
+  currentPlayer = random(players);
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      avail.push([i, j])
+
+    }
+  }
+}
+function mousePressed() {
+  nextTurn();
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      let box = board[i][j];
+      let x = w * i + w / 2;
+      let y = h * j + h / 2;
+      textSize(32);
+      strokeWeight(4);
+      if (box === players[0]) {
+        ellipse(x, y, w / 2)
+      }
+      else if (box === players[1]) {
+        let xr = w / 4;
+        line(x - xr, y - xr, x + xr, y + xr);
+        line(x + xr, y - xr, x - xr, y + xr);
+      }
+      text(box, x, y);
+
+    }
+
+}
+
+function equals3(a, b, c) {
+  return (a == b && b == c && a == c && a !== '');
+}
+
+function checkWinner() {
+  let winner = null;
+
+  // horizontal
+  for (let i = 0; i < 3; i++) {
+    if (equals3(board[i][0], board[i][1], board[i][2])) {
+      winner = board[i][0];
+    }
+  }
+  //Vertical
+  for (let i = 0; i < 3; i++) {
+    if (equals3(board[0][i], board[0][i], board[0][i])) {
+      winner = board[0][i];
+    }
+  }
+  //Diagonal
+  if (equals3(board[0][0], board[1][1], board[1][1])) {
+    winner = board[0][0];
+  }
+  if (equals3(board[2][0], board[1][1], board[0][2])) {
+    winner = board[2][0];
+  }
+
+
+  if (winner == null && avail.length === 0) {
+    return 'tie'
+  } else {
+    return 'winner'
+  }
+
 }
 
 
-function draw(){
-  if(playerCount === 4){
-    game.update(1);
-  }
-  if(gameState === 1){
-    clear();
-    game.play();
-  }
-  if(gameState === 2){
-    game.end();
+
+function nextTurn() {
+  let index = (random(avail.length));
+  let spot = avail.splice(index, 1)[0];
+  let i = spot[0];
+  let j = spot[1];
+  board[i][j] = players[currentPlayer];
+  currentPlayer = (currentPlayer + 1) % players.length;
+}
+
+
+
+function draw() {
+  background("white");
+  let w = width / 3;
+  let h = height / 3;
+
+  line(w, 0, w, height);
+  line(w * 2, 0, w * 2, height);
+  line(0, h, width, h);
+  line(0, h * 2, width, h * 2);
+
+  
+
+
+   
+
+    let result = checkWinner();
+
+    // if (result != null) {
+    //   noLoop();
+    //   console.log(result);
+    // }
+    //nextTurn();
+  
   }
 }
